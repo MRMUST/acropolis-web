e312003c22b064c0e2961085fd9de357ee6db756
must <65069351+MRMUST@users.noreply.github.com>
2020-12-14 12:21:35 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("잠시만요!")
+    console.log("당신이 지금 저가 어떤 꼬라지로 사이트를 만들었는지 궁금  하겠지만")
+    console.log("참으세요!")
+    console.log("저의 코딩은 개판이기 때문입니다. 이럴바엔 다른 사이트를 참고 해보시는건 어떤가요?")
+    function button1_click() {
+    location.href="https://discord.com/api/oauth2/authorize?client_id=756874789846777957&permissions=8&scope=bot"
+}
